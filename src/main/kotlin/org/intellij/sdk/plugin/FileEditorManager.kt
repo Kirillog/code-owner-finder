84bf1086151f81d1f55f2ad5dc210b49a4787d3f
Kirillog <kirill.mitkin.03@gmail.com>
2022-03-08 19:18:34 +0300

    add file editor listener

---
+++ src/main/kotlin/org/intellij/sdk/plugin/FileEditorManager.kt
@@ -0,0 +1,17 @@
+package org.intellij.sdk.plugin
+
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent
+import com.intellij.openapi.fileEditor.FileEditorManagerListener
+import com.intellij.openapi.project.Project
+import org.intellij.sdk.plugin.toolWindow.reloadContent
+
+class FileEditorManager(private val project: Project) :
+    FileEditorManagerListener {
+
+    /**
+     * Reloads content of tool window on selectionChanged [event]
+     */
+    override fun selectionChanged(event: FileEditorManagerEvent) {
+        reloadContent(project, event.newFile)
+    }
+}
