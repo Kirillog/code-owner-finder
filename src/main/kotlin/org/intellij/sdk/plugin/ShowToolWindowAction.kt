9ba41a520ac1336a44992373e2e4879c505d6780
Kirillog <kirill.mitkin.03@gmail.com>
2022-03-10 00:24:35 +0300

    add showing toolWindow after action performing

--- src/main/kotlin/org/intellij/sdk/plugin/ShowToolWindowAction.kt
+++ src/main/kotlin/org/intellij/sdk/plugin/ShowToolWindowAction.kt
@@ -4,14 +4,15 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.project.Project
-import org.intellij.sdk.plugin.toolWindow.reloadContent
+import org.intellij.sdk.plugin.toolWindow.ToolWindowManager
 
 
-class CodeOwnerFinderAction : AnAction() {
+class ShowToolWindowAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
         val file = event.getRequiredData(CommonDataKeys.VIRTUAL_FILE)
         val project: Project = event.getRequiredData(CommonDataKeys.PROJECT)
-        reloadContent(project, file)
+        ToolWindowManager.reloadContent(project, file)
+        ToolWindowManager.show(project)
     }
 
     override fun update(event: AnActionEvent) {
